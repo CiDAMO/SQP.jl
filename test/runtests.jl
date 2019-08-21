1a32b8a9f5a496e49aa8b8a6f47f034716cbc0bc
rodrigosgr <32708394+rodrigosgr@users.noreply.github.com>
2019-08-21 13:04:49 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,10 @@ using LinearAlgebra
 
 function test()
   @testset "teste que falha" begin
-    f1(x) = -x[1]*x[2]*x[3]
+    f1(x) = (x[1] - 2x[3]*(1 - x[2]))^4 + (x[2] - x[1])^2
     x1 = [1.0, 1.0, 1.0]
-    c1(x) = [x[1]*x[2] + 2*x[1]*x[3] + 2*x[2]*x[3] - 12]
-    nlp1 = ADNLPModel(f1, x1, c = c1, ucon = [0], lcon = [0])
+    c1(x) = [x[1] * x[3], x[1] + x[3]^3 - x[2] * x[1]]
+    nlp1 = ADNLPModel(f1, x1, c = c1, ucon = [1.0, 2.0], lcon = [1.0, 2.0])
     sol1 = with_logger(NullLogger()) do
       sqp(nlp1)
     end
