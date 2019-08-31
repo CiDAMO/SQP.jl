2448566bcdcf649c8603101de359209ce8bf1e24
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-08-31 20:52:32 -0300

    Add tests and update travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,50 @@
 using SQP
 
-using Test
+using JSOSolvers, LinearAlgebra, Logging, SparseArrays, Test
+
+const jsosolvers_folder = joinpath(dirname(pathof(JSOSolvers)), "..", "test", "solvers")
+include(joinpath(jsosolvers_folder, "unconstrained.jl"))
 
 using NLPModels
-using LinearAlgebra
 
 function test()
-  @testset "teste 1" begin
-    f1(x) = (x[1] - 2x[3]*(1 - x[2]))^4 + (x[2] - x[1])^2
-    x1 = [1.0, 1.0, 1.0]
-    c1(x) = [x[1] * x[3], x[1] + x[3]^3 - x[2] * x[1]]
-    nlp1 = ADNLPModel(f1, x1, c = c1, ucon = [1.0, 2.0], lcon = [1.0, 2.0])
-    sol1 = with_logger(NullLogger()) do
-      sqp(nlp1)
+  # unconstrained tests from JSOSolvers
+  # test_unconstrained_solver(sqp)
+
+  @testset "Small equality constrained problems" begin
+    for (x0, m, f, c, sol) in [([1.0; 2.0], 1,
+                                x -> 2x[1]^2 + x[1] * x[2] + x[2]^2 - 9x[1] - 9x[2],
+                                x -> [4x[1] + 6x[2] - 10],
+                                ones(2)
+                               ),
+                               ([-1.2; 1.0], 1,
+                                x -> (x[1] - 1)^2,
+                                x -> [10 * (x[2] - x[1]^2)],
+                                ones(2)
+                               ),
+                               ([-1.2; 1.0], 1,
+                                x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2,
+                                x -> [(x[1] - 2)^2 + (x[2] - 2)^2 - 2],
+                                ones(2)
+                               ),
+                               ([2.0; 1.0], 2,
+                                x -> -x[1],
+                                x -> [x[1]^2 + x[2]^2 - 25; x[1] * x[2] - 12],
+                                [4.0; 3.0]
+                               )
+                              ]
+      nlp = ADNLPModel(f, x0, c=c, lcon=zeros(m), ucon=zeros(m))
+      output = with_logger(NullLogger()) do
+        sqp(nlp)
+      end
+
+      @test isapprox(output.solution, sol, rtol=1e-6)
+      @test output.primal_feas < 1e-6
+      @test output.dual_feas < 1e-6
+      @test output.status == :first_order
     end
-    @test sol1.primal_feas < 1e-8
-    @test sol1.dual_feas < 1e-8
-    @test sol1.elapsed_time < 30
-    @test sol1.iter < 1000
   end
+
 end
 
 test()
