f095f0efb865e2169afb1e92ee616cbd6758db72
rodrigoredondo <rodrigoredondo0@gmail.com>
2019-10-21 10:19:22 -0200

    atualizacao dos nomes de param/var

--- src/SQP.jl
+++ src/SQP.jl
@@ -11,21 +11,24 @@ using SolverTools
 using JSOSolvers
 
 
-function sqp(nlp;
-             precision = 1e-8,
-             max_iter = 1000,
-             time_lim = 30.0,
-             relax_param = 0.5,
-             trust_reg = 3.0,
-             μ0 = 1.0)
+function sqp(nlp :: AbstractNLPModel;
+             atol :: Real = 1e-8,
+             rtol :: Real = 1e-8,
+             max_eval :: Int = -1,
+             cons_tol :: Real = 1e-8,
+             max_iter :: Int = 1000,
+             max_time :: Float64 = 30.0,
+             relax_param :: Float64 = 0.5,
+             trust_reg :: Float64 = 3.0,
+             μ0 :: Float64 = 1.0)
 
 
     if bound_constrained(nlp)
-        return tron(nlp, x = copy(nlp.meta.x0), max_time = time_lim, atol = precision)
+        return tron(nlp, x = copy(nlp.meta.x0), max_time = max_time, atol = atol, rtol = rtol, max_eval = max_eval)
     end
 
     if unconstrained(nlp)
-        return trunk(nlp, x = copy(nlp.meta.x0), max_time = time_lim, nm_itmax = max_iter, atol = precision)
+        return trunk(nlp, x = copy(nlp.meta.x0), max_time = max_time, nm_itmax = max_iter, atol = atol, rtol = rtol, max_eval = max_eval)
     end
 
     if equality_constrained(nlp) == false || has_bounds(nlp) == true
@@ -33,7 +36,7 @@ function sqp(nlp;
     end
 
     iter = 1
-    time_0 = time()
+    start_time = time()
     n = nlp.meta.nvar
     m = nlp.meta.ncon
     x = copy(nlp.meta.x0)
@@ -54,23 +57,27 @@ function sqp(nlp;
     last_but_one_rejected = false
 
     exitflag = :unknow
-    norm_first = norm(A'*y - gx)
-    success = norm_first < precision && norm_cx < precision
+    dual = A'*y - gx
+    normdual = norm(dual)
+    tol = atol + rtol * normdual
+    success = normdual < tol && norm_cx < cons_tol
     if success
         exitflag = :first_order
     end
-    now = time() - time_0
-    tired = now > time_lim || iter > max_iter
+    Δt = time() - start_time
+    tired = Δt > max_time || iter > max_iter || neval_obj(nlp) > max_eval > 0
     if tired
-         if now > time_lim
+         if Δt > max_time
             exitflag = :max_time
-        else
+        elseif iter > max_iter
             exitflag = :max_iter
+        else
+            exitflag = :max_eval
         end
     end
 
     @info log_header([:iter, :time, :dual, :normcx, :radius, :ratio], [Int, Float64, Float64, Float64, Float64, Float64])
-    @info log_row(Any[iter, now, norm_first, norm_cx, tr.radius, ρ])
+    @info log_row(Any[iter, Δt, normdual, norm_cx, tr.radius, ρ])
 
     while !(success || tired)
         v = lsmr(A, -cx, radius = relax_param * tr.radius)[1]
@@ -91,7 +98,7 @@ function sqp(nlp;
         if μ_plus > μ && μ_plus < 5*μ && μ > last_accepted_μ && norm_cx > 0.2*last_accepted_norm_c && (last_rejected || last_but_one_rejected)
             μ_plus = min(5*μ, μ_plus + 25*(μ_plus - last_accepted_μ))
         end
-        if μ_plus == μ && norm(v) < relax_param * tr.radius/10 && norm_cx < 1e4 * precision
+        if μ_plus == μ && norm(v) < relax_param * tr.radius/10 && norm_cx < 1e4 * tol
             μ_plus = max(μ0, μ_bar, norm(y))
         end
 
@@ -125,29 +132,32 @@ function sqp(nlp;
 
         update!(tr, norm(d))
 
-        norm_first = norm(A'*y - gx)
-        success = norm_first < precision && norm_cx < precision
+        dual = A'*y - gx
+        normdual = norm(dual)
+        success = normdual < tol && norm_cx < cons_tol
         if success
             exitflag = :first_order
         end
-        now = time() - time_0
+        Δt = time() - start_time
         iter += 1
-        tired = now > time_lim || iter > max_iter
+        tired = Δt > max_time || iter > max_iter || neval_obj(nlp) > max_eval > 0
         if tired
-            if now > time_lim
+            if Δt > max_time
                 exitflag = :max_time
-            else
+            elseif iter > max_iter
                 exitflag = :max_iter
+            else
+                exitflag = :max_eval
             end
         end
 
-        @info log_row(Any[iter, now, norm_first, norm_cx, tr.radius, ρ])
+        @info log_row(Any[iter, Δt, normdual, norm_cx, tr.radius, ρ])
 
     end
 
     cx = cx + nlp.meta.ucon
 
-    return GenericExecutionStats(exitflag, nlp, solution = x, objective = fx, dual_feas = norm_first, primal_feas = norm_cx, iter = iter, elapsed_time = now)
+    return GenericExecutionStats(exitflag, nlp, solution = x, objective = fx, dual_feas = normdual, primal_feas = norm_cx, iter = iter, elapsed_time = Δt)
 
 end
 
