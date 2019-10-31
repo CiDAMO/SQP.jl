ddbab71af05adbc93ce2678c73f00a66cf52627c
rodrigoredondo <rodrigoredondo0@gmail.com>
2019-10-30 23:34:53 -0200

    Z matrix problem fixed and changes in ared-pred and relax_param

--- src/SQP.jl
+++ src/SQP.jl
@@ -18,8 +18,8 @@ function sqp(nlp :: AbstractNLPModel;
              cons_tol :: Real = 1e-8,
              max_iter :: Int = 1000,
              max_time :: Float64 = 30.0,
-             relax_param :: Float64 = 0.5,
-             trust_reg :: Float64 = 3.0,
+             relax_param :: Float64 = 0.8,
+             trust_region_radius :: Float64 = 3.0,
              μ0 :: Float64 = 1.0)
 
 
@@ -32,7 +32,7 @@ function sqp(nlp :: AbstractNLPModel;
     end
 
     if equality_constrained(nlp) == false || has_bounds(nlp) == true
-        error("This SQP implementation only works for equality constraints and unbounded variables")
+        error("This SQP implementation only works for equality cons.aints and unbounded variables")
     end
 
     iter = 1
@@ -48,7 +48,6 @@ function sqp(nlp :: AbstractNLPModel;
     W = Symmetric(hess(nlp, x, y = y), :L)
     Z = nullspace(Matrix(A))
     norm_cx = norm(cx)
-    tr = TrustRegion(trust_reg)
     ρ = 0.0
     μ = μ0
     last_accepted_μ = μ0 # for the first iteration
@@ -77,13 +76,13 @@ function sqp(nlp :: AbstractNLPModel;
     end
 
     @info log_header([:iter, :time, :dual, :normcx, :radius, :ratio], [Int, Float64, Float64, Float64, Float64, Float64])
-    @info log_row(Any[iter, Δt, normdual, norm_cx, tr.radius, ρ])
+    @info log_row(Any[iter, Δt, normdual, norm_cx, trust_region_radius, ρ])
 
     while !(success || tired)
-        v = lsmr(A, -cx, radius = relax_param * tr.radius)[1]
+        v = lsmr(A, -cx, radius = relax_param * trust_region_radius)[1]
         ZWZ = Z' * W * Z
         ZWv = Z' * (W*v + gx)
-        u = cg(ZWZ, -ZWv, radius = sqrt(tr.radius^2 - norm(v)^2))[1]
+        u = cg(ZWZ, -ZWv, radius = sqrt(trust_region_radius^2 - norm(v)^2))[1]
         d = v + Z * u
 
         next_x = x + d
@@ -98,39 +97,39 @@ function sqp(nlp :: AbstractNLPModel;
         if μ_plus > μ && μ_plus < 5*μ && μ > last_accepted_μ && norm_cx > 0.2*last_accepted_norm_c && (last_rejected || last_but_one_rejected)
             μ_plus = min(5*μ, μ_plus + 25*(μ_plus - last_accepted_μ))
         end
-        if μ_plus == μ && norm(v) < relax_param * tr.radius/10 && norm_cx < 1e4 * tol
+        if μ_plus == μ && norm(v) < relax_param * trust_region_radius/10 && norm_cx < 1e4 * tol
             μ_plus = max(μ0, μ_bar, norm(y))
         end
 
-        ϕ(x) = obj(nlp, x) + μ_plus * norm(cons(nlp, x))
-        nlp_aux = ADNLPModel(ϕ, x)
-        ϕx = fx + μ_plus * norm_cx
-        ϕn = next_f + μ_plus * next_norm_c
-        Δm = μ * vpred - upred
-
-        ared, pred = aredpred(nlp_aux, ϕn, ϕx, Δm, next_x, d, dot(d, grad(nlp_aux,x)))
+        ared = fx - next_f + μ_plus * (norm_cx - next_norm_c) + 1e-32 #1e-32 to avoid NaN
+        pred = -0.5 * d'*W*d - dot(gx,d) + μ_plus * vpred + 1e-32
         ρ = ared / pred
-        set_property!(tr, :ratio, ρ)
 
-        if acceptable(tr)
+        if ρ > 0
             x = next_x
             fx = next_f
             cx = next_c
             norm_cx = next_norm_c
+            if norm_cx <= 1e-2
+                relax_param = 0.2
+            end
             A = jac(nlp, x)
             gx = grad(nlp, x)
             y = lsmr(A', gx)[1]
             W = Symmetric(hess(nlp, x, y = y), :L)
             Z = nullspace(Matrix(A))
             last_but_one_rejected = last_rejected
-            last_rejected = true
+            last_rejected = false
             last_accepted_μ = μ
             last_accepted_norm_c = norm_cx
+            if ρ >= 0.75
+              trust_region_radius = trust_region_radius * 1.5
+            end
+        else
+            trust_region_radius = trust_region_radius / 1.5
+            last_rejected = true
         end
-
         μ = μ_plus
-
-        update!(tr, norm(d))
 
         dual = A'*y - gx
         normdual = norm(dual)
@@ -151,7 +150,7 @@ function sqp(nlp :: AbstractNLPModel;
             end
         end
 
-        @info log_row(Any[iter, Δt, normdual, norm_cx, tr.radius, ρ])
+        @info log_row(Any[iter, Δt, normdual, norm_cx, trust_region_radius, ρ])
 
     end
 
