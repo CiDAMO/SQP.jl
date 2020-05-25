5aad1cd8f87bf67a086382cdad302658dd54c790
rodrigosgr <rodrodrigoredondo0@gmail.com>
2020-05-25 18:14:51 -0300

    second order correction

--- src/SQP.jl
+++ src/SQP.jl
@@ -12,14 +12,14 @@ using JSOSolvers
 
 
 function sqp(nlp :: AbstractNLPModel;
-             atol :: Real = 1e-8,
-             rtol :: Real = 1e-8,
+    	     x :: AbstractVector = copy(nlp.meta.x0),
+             atol :: Real = sqrt(eps(eltype(x))),
+             rtol :: Real = sqrt(eps(eltype(x))),
              max_eval :: Int = -1,
-             cons_tol :: Real = 1e-8,
-             max_iter :: Int = 1000,
+             max_iter :: Int = 25,
              max_time :: Float64 = 30.0,
              relax_param :: Float64 = 0.6,
-             trust_reg :: Float64 = 3.0,
+             trust_reg :: Float64 = 1.0,
              μ0 :: Float64 = 1.0)
 
 
@@ -35,11 +35,10 @@ function sqp(nlp :: AbstractNLPModel;
         error("This SQP implementation only works for equality constraints and unbounded variables")
     end
 
-    iter = 1
+    iter = 0
     start_time = time()
     n = nlp.meta.nvar
     m = nlp.meta.ncon
-    x = copy(nlp.meta.x0)
     y = copy(nlp.meta.y0)
     fx = obj(nlp, x)
     cx = cons(nlp, x) - nlp.meta.ucon
@@ -59,8 +58,7 @@ function sqp(nlp :: AbstractNLPModel;
     exitflag = :unknow
     dual = A'*y - gx
     normdual = norm(dual)
-    tol = atol + rtol * normdual
-    success = normdual < tol && norm_cx < cons_tol
+    success = normdual <  atol + rtol * normdual && norm_cx < atol + rtol * norm_cx
     if success
         exitflag = :first_order
     end
@@ -80,11 +78,12 @@ function sqp(nlp :: AbstractNLPModel;
     @info log_row(Any[iter, Δt, normdual, norm_cx, tr.radius, ρ])
 
     while !(success || tired)
-        v = lsmr(A, -cx, radius = relax_param * tr.radius)[1]
+        v = lsmr(A, -cx, radius = relax_param * tr.radius, atol = atol, rtol = rtol, itmax = max(2 * n, 50))[1]
         ZWZ = Z' * W * Z
         ZWv = Z' * (W*v + gx)
-        u = cg(ZWZ, -ZWv, radius = sqrt(tr.radius^2 - norm(v)^2))[1]
-        d = v + Z * u
+        u = cg(ZWZ, -ZWv, radius = sqrt(tr.radius^2 - norm(v)^2), atol = atol, rtol = 0.0, itmax = max(2 * n, 50))[1]
+	Zu = Z * u
+        d = v + Zu
 
         next_x = x + d
         next_f = obj(nlp, next_x)
@@ -95,10 +94,10 @@ function sqp(nlp :: AbstractNLPModel;
         μ_bar = 0.1 + upred / vpred # auxiliary variable for μ update
 
         μ_plus = max(μ, μ_bar) # another auxiliary variable
-        if μ_plus > μ && μ_plus < 5*μ && μ > last_accepted_μ && norm_cx > 0.2*last_accepted_norm_c && (last_rejected || last_but_one_rejected)
-            μ_plus = min(5*μ, μ_plus + 25*(μ_plus - last_accepted_μ))
+        if μ_plus > μ && μ_plus < 5 * μ && μ > last_accepted_μ && norm_cx > 0.2 * last_accepted_norm_c && (last_rejected || last_but_one_rejected)
+            μ_plus = min(5 * μ, μ_plus + 25 * (μ_plus - last_accepted_μ))
         end
-        if μ_plus == μ && norm(v) < relax_param * tr.radius/10 && norm_cx < 1e4 * tol
+        if μ_plus == μ && norm(v) < relax_param * tr.radius/10 && norm_cx < 1e4 * atol
             μ_plus = max(μ0, μ_bar, norm(y))
         end
 
@@ -109,10 +108,14 @@ function sqp(nlp :: AbstractNLPModel;
         Δm =  μ * vpred - upred
 
         ared, pred = aredpred(nlp, ϕn, ϕx, Δm, next_x, d, dot(d, gx) - μ_plus * norm_cx)
-        ρ = (ared  + tol) / (pred + tol)
+        ρ = ared / pred
         set_property!(tr, :ratio, ρ)
 
-        if acceptable(tr)
+	norm_v = norm(v)
+	norm_Zu = norm(Zu)
+
+
+        if acceptable(tr) 
             x = next_x
             fx = next_f
             cx = next_c
@@ -123,9 +126,38 @@ function sqp(nlp :: AbstractNLPModel;
             W = Symmetric(hess(nlp, x, y), :L)
             Z = nullspace(Matrix(A))
             last_but_one_rejected = last_rejected
-            last_rejected = true
+            last_rejected = false
             last_accepted_μ = μ
             last_accepted_norm_c = norm_cx
+	elseif norm_v < 0.8 * relax_param * tr.radius && norm_v < 0.1 * norm_Zu
+		aux_sist = cg(A*A', next_c)[1]
+		d -= A'*aux_sist
+		next_x = x + d
+		next_f = obj(nlp, next_x)
+		next_cx = cons(nlp, next_x) - nlp.meta.ucon
+		next_norm_c = norm(next_cx)
+   		ϕx = fx + μ_plus * norm_cx
+        		ϕn = next_f + μ_plus * next_norm_c
+        	Δm =  μ * vpred - upred
+		ared, pred = aredpred(nlp, ϕn, ϕx, Δm, next_x, d, dot(d, gx) - μ_plus * norm_cx)
+        	ρ = ared / pred
+        	set_property!(tr, :ratio, ρ)
+		x = next_x
+            	fx = next_f
+            	cx = next_c
+            	norm_cx = next_norm_c
+		A = jac(nlp, x)
+            	gx = grad(nlp, x)
+            	y = lsmr(A', gx)[1]
+         	W = Symmetric(hess(nlp, x, y), :L)
+       	     	Z = nullspace(Matrix(A))
+            	last_but_one_rejected = last_rejected
+            	last_rejected = false
+            	last_accepted_μ = μ
+            	last_accepted_norm_c = norm_cx
+	else
+		last_but_one_rejected = last_rejected
+            	last_rejected = true
         end
 
         μ = μ_plus
@@ -134,7 +166,7 @@ function sqp(nlp :: AbstractNLPModel;
 
         dual = A'*y - gx
         normdual = norm(dual)
-        success = normdual < tol && norm_cx < cons_tol
+        success = normdual < atol + rtol * normdual && norm_cx < atol + rtol * norm_cx
         if success
             exitflag = :first_order
         end
